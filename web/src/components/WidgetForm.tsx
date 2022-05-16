5e458feee3fc682780bbbdee36c2e4242b2a1b41
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-05-16 14:11:58 -0300

    Project created

---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,70 @@
+import { FaHeart } from 'react-icons/fa'
+import { CloseButton } from './CloseButton'
+import bugImageUrl from '../assets/bug.svg'
+import ideaImageUrl from '../assets/idea.svg'
+import thoughtImageUrl from '../assets/thought.svg'
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Idéia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  }
+}
+
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button>
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          )
+        })}
+      </div>
+      <p>
+        {' '}
+        <a target="_blank" href="https://github.com/thmoreiracosta">
+          {' '}
+          Thiago Costa{' '}
+        </a>
+      </p>
+
+      <footer className="text-xs text-neutral-400 flex space-x-1">
+        Feito com <FaHeart className="text-[#B51942] w-4 m-1" />
+        pela
+        <a
+          className="underline underline-offset-2"
+          target="_blank"
+          href="https://rocketseat.com.br "
+        >
+          Rocketseat.
+        </a>
+      </footer>
+    </div>
+  )
+}
